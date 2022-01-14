e5ba7a78e3710812251a7d6972ef1ca90f0f45f3
Walber Santos <walber_santos@hotmail.com>
2022-01-14 08:55:01 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/wcomservice/wcomservice/WcomserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wcomservice.wcomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcomserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcomserviceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wcomservice/wcomservice/WcomserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcomservice.wcomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcomserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
